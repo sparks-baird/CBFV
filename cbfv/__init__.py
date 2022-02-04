1780147e667890b20e03f15d45964deb55a73814
sgbaird <sterling.baird@icloud.com>
2022-02-03 23:13:25 -0700

    Update __init__.py

--- cbfv/__init__.py
+++ cbfv/__init__.py
@@ -1,3 +1,3 @@
 """Tool for quickly creating a composition-based feature vector."""
-__version__ = "1.0.2"
-__author__ = 'Steven Kauwe, Andrew Falkowski, Anthony Wang, Sterling G. Baird'
+__version__ = "1.0.3"
+__author__ = "Steven Kauwe, Andrew Falkowski, Anthony Wang, Sterling G. Baird"
