cb389b920678a404f399064f812a0f735f7d4f55
sgbaird <sterling.baird@utah.edu>
2023-06-26 18:05:06 -0600

    bump

--- composition_based_feature_vector/__init__.py
+++ composition_based_feature_vector/__init__.py
@@ -1,3 +1,3 @@
 """Tool for quickly creating a composition-based feature vector."""
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __author__ = "Steven Kauwe, Andrew Falkowski, Anthony Wang, Sterling G. Baird"
